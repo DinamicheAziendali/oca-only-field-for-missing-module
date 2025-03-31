e67a953d98942f4e1903a8c247e4ce4041442341
gianmarco <gconte@dinamicheaziendali.it>
2025-03-31 12:02:19 +0200

    ADD account_tax_kind

--- l10n_it_account_tax_kind/__manifest__.py
+++ l10n_it_account_tax_kind/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "ITA - Natura delle aliquote IVA",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.1",
     "development_status": "Production/Stable",
     "category": "Localization/Italy",
     "summary": "Gestione natura delle aliquote IVA",

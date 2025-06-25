f4d0667f296ab021927b172f1200366cc4049f4f
gianmarco <gconte@dinamicheaziendali.it>
2025-06-25 16:55:25 +0200

    [MIG] Pez8 per mig 18

---
+++ stock_close_period_mrp/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2023-Today:
+# Dinamiche Aziendali Srl (<http://www.dinamicheaziendali.it/>)
+# @author: Marco Calcagni <mcalcagni@dinamicheaziendali.it>
+# @author: Giuseppe Borruso <gborruso@dinamicheaziendali.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Close Period - MRP",
+    "summary": "Stock Close Period - MRP",
+    "version": "18.0.1.0.0",
+    "category": "Stock",
+    "author": "Pordenone Linux User Group (PNLUG), Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "license": "AGPL-3",
+    "depends": [
+        "stock_close_period",
+        "mrp",
+        "mrp_account",
+    ],
+    "data": [
+        # "views/stock_close_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}

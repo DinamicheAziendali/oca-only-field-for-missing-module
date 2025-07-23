35665b425fb42ac115dd608ca01a1ecfc6c4103a
Borruso <gborruso@dinamicheaziendali.it>
2025-07-23 15:58:16 +0200

    [REF] l10n_it_fatturapa: decommentati camoi

--- l10n_it_fatturapa/models/account.py
+++ l10n_it_fatturapa/models/account.py
@@ -137,8 +137,8 @@ class DiscountRisePrice(models.Model):
     _description = "E-invoice Discount Supplement Data"
 
     name = fields.Selection([("SC", "Discount"), ("MG", "Supplement")], "Type")
-#     percentage = fields.Float()
-#     amount = fields.Float(digits="Discount")
+    percentage = fields.Float()
+    amount = fields.Float(digits="Discount")
     invoice_line_id = fields.Many2one(
         "account.move.line",
         "Related Invoice from line",

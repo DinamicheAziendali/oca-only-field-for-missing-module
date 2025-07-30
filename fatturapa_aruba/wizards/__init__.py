ba964f9d544f1f35ca52ff77f0718c41496bd8d9
gianmarco <gconte@dinamicheaziendali.it>
2025-07-30 11:12:18 +0200

    rimosso fatturapa_aruba

--- fatturapa_aruba/wizards/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2019-TODAY  Openforce Srls Unipersonale (www.openforce.it)
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-
-from . import wizard_export_fatturapa
-from . import wizard_import_fatturapa_xls
-from . import wizard_import_fatturapa
-from . import wizard_send_multi_fatturapa
